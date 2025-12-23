56cb7e5e9e24844e1d8e20f6fe742f1eb57e3bf1
Abhiraj212024 <abhirajraje1@gmail.com>
2025-12-24 00:27:57 +0530

    completed tutorial 7

--- 07-tut/server.js
+++ 07-tut/server.js
@@ -1,16 +1,28 @@
 const path = require('path')
-const logEvents = require('./middleware/logEvents')
+const cors = require('cors')
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3500
 
-//custom middleware for logging
-app.use((req, res, next) => {
-    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt')
-    console.log(`${req.method} ${req.path}`)
-    next()
-})
 
+//custom middleware for logging
+app.use(logger)
+
+//Cross Origin Resource Sharing
+const whiteList = ['https://www.google.com', 'http://localhost:3500', 'http://127.0.0.1:3500', 'http://127.0.0.1:5500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin){ // remove !origin after develoment
+            callback(null, true)
+        }else{
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // built in middleware to handle url encoded data
 // in other words: form data:
@@ -21,8 +33,7 @@ app.use(express.urlencoded({extended: false}))
 //built in middleware for json
 app.use(express.json())
 
-// built in middleware for static files
-
+// built in middleware for serving static files like CSS, images, etc
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.get(/^\/$/, (req, res) => {
@@ -68,8 +79,19 @@ const three = (req, res) => {
 
 app.get(/^\/chain(\.html)?$/, [one, two, three])
 
-app.get(/\/*/, (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all(/^*$/, (req, res) => {
+    res.status(404);
+    if (req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        
+    }else if (req.accepts('json')){
+        res.json({error: "404 Not Found"})
+
+    }else{
+        res.type('txt').send("404 Not Found")
+    }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {console.log(`Server running on Port ${PORT}`)})
